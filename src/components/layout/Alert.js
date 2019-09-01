8b37130c7d9d1284ab632c999bee52dbcaf0d2b4
zwglass <zwglasses@gmail.com>
2019-09-02 07:39:25 +0800

    programing alert

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,10 +1,18 @@
 import React, { useContext } from 'react'
 import AlertContext from '../../contexts/alert/alertContext';
+// import React, { useState, useEffect } from 'react'
 
 const Alert = () => {
     /** alert={msg:'', type:''} type=[primary,light,dark,danger,success,white] */
     const alertContext = useContext(AlertContext);
     const { alert } = alertContext;
+
+    // const [alert, setAlert] = useState(null);
+
+    // useEffect(() => {
+    //     setAlert(alertObj);
+    // }, [alertObj]);
+
     return (
         alert !== null && (
             <div className={`alert alert-${alert.type}`}>
